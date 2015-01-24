175e07e884df838bd0f3013ad1f705c37bc37bcb
Svetoslav Popov <svetoslavpopov93@gmail.com>
2015-01-24 10:02:07 +0200

    Importing MvcMusicStore project

---
+++ MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "I love cake!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Sample()
+        {
+            ViewBag.Message = "Hello World. Welcome to ASP.NET MVC!";
+            return View("Sample");
+        }
+    }
+}
